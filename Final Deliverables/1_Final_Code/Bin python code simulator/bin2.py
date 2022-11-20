461cd2d530123e4dac4e397852247e101fc5f213
SManojKumar1024 <115528108+SManojKumar1024@users.noreply.github.com>
2022-11-20 07:25:59 +0530

    Update bin2.py

--- Final Deliverables/1_Final_Code/Bin python code simulator/bin2.py
+++ Final Deliverables/1_Final_Code/Bin python code simulator/bin2.py
@@ -8,11 +8,11 @@ import sys
 
 # watson device details
 
-organization = "4yi0vc"
-devicType =  "BIN2"
-deviceId = "BIN2ID"
+organization = "1qvr56"
+devicType =  "NODE"
+deviceId = "12345"
 authMethod= "token"
-authToken= "123456789"
+authToken= "5N959hF&_r_G-!rjPy"
 
 #generate random values for randomo variables (temperature&humidity)
 
